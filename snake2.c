3be6df4bf899fbf1eda5304961b855b181ccbdb9
Luke <43900997+TyrantLuke@users.noreply.github.com>
2018-12-05 19:01:36 +0800

    +双缓冲
    win10环境下改写，
    下一步：简化代码，加入游戏边框说明，改进符号表示

--- snake2.c
+++ snake2.c
@@ -1,210 +1,254 @@
 #include<stdio.h>
 #include<stdlib.h>        
-#include<unistd.h>
-#include<termios.h>
-#include<time.h>           
-#include<fcntl.h>
+#include<conio.h>      
+#include<windows.h>
+
+//定义四个方向的对应字符
 #define up 'w'
 #define down 's'
-#define left 'a'                         //定义四个方向的对应字符
+#define left 'a'                         
 #define right 'd'
 
 #define SN sizeof(struct snake)
-
-struct snake                              //结构体
+struct snake//结构体,内含蛇节点的坐标和其前、后节点的指针；
 {
-    int s_y;
-    int s_x;
-    struct snake * next;
-};
-struct snake *head,*p1,*p2; 
+	int s_y;
+	int s_x;
+	struct snake * next;
+	struct snake * formal;
+}*head, *p1, *p2;
+
+HANDLE hOutput, hOutBuf;//控制台屏幕缓冲区句柄
+COORD coord = { 0,0 };
+//双缓冲处理显示
+DWORD bytes = 0;
+
 
 char dir;                               //决定蛇头接下来的方向
 char formal_dir;                         //记忆蛇头原来的方向
-int li,num,f1,f2,b,a,y,x;                     
+int li, num, f1, f2, b, a, y, x;
+char map2[20][20];
+int bgd[20][20] = 
+			  { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+				1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+				1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+				1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+				1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+				1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,
+				1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+				1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+				1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+				1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+				1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+				1,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,1,
+				1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+				1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+				1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+				1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+				1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+				1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+				1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+				1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 };//加墙地图 
 
-int bgd[20][20]={1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-                1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
-                1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,
-                1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,                    
-                1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
-                1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
-                1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
-                1,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,1,
-                1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
-                1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
-                1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
-                1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
-                1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};//加墙地图 
+
 
 
 int main()
 {
-    void food();//生成食物
-    int kbhit(); //响应键盘敲击（在win中有头文件conio.h，但在Linux中没有，故此处为自行定义）
-    int agetch();//得到敲入值
-    int usleep();//休眠函数
-    void direction();//处理输入数据，转为蛇的走向
-    head=p1=p2=(struct snake*)malloc(SN);            
-    head->s_y=4;
-    head->s_x=4;
-    head->next=NULL;
+	//用双缓冲解决闪屏问题；
+	//创建新的控制台缓冲区
+	hOutBuf = CreateConsoleScreenBuffer(
+		GENERIC_WRITE,//定义进程可以往缓冲区写数据
+		FILE_SHARE_WRITE,//定义缓冲区可共享写权限
+		NULL,
+		CONSOLE_TEXTMODE_BUFFER,
+		NULL
+	);
+	hOutput = CreateConsoleScreenBuffer(
+		GENERIC_WRITE,//定义进程可以往缓冲区写数据
+		FILE_SHARE_WRITE,//定义缓冲区可共享写权限
+		NULL,
+		CONSOLE_TEXTMODE_BUFFER,
+		NULL
+	);
+	//隐藏两个缓冲区的光标
+	CONSOLE_CURSOR_INFO cci;
+	cci.bVisible = 0;
+	cci.dwSize = 1;
+	SetConsoleCursorInfo(hOutput, &cci);
+	SetConsoleCursorInfo(hOutBuf, &cci);
 
-    formal_dir=dir='s',li=1;//初始方向向下，生命值为1
+	void food();//生成食物
+	void change_map2();
+	void direction();//处理输入数据，转为蛇的走向
+	void move();//根据蛇咬到了啥决定运动，伸长，结束游戏等。
+	void print1();//用来展示缓冲区1
+	void print2();//用来展示缓冲区2
+	head = p1 = p2 = (struct snake*)malloc(SN);//动态分配内存
+	head->s_y = 4;//初始位置为（4,4）
+	head->s_x = 4;
+	head->next = NULL;
+	head->formal = NULL;
+	srand((unsigned int)(time_t(NULL)));//将随机数种子设为系统时间
+	f1 = rand() % 18 + 1;//f代表food，f1和f2分别表示食物的纵，横坐标
+	srand(f1);
+	f2 = rand() % 18 + 1;
 
-    b=4,a=4;
-    num=0;
-    food();
-    while(1)
-    {
-         
-        if(kbhit())
-            dir=agetch();
-        direction();
-        
-        if(bgd[b][a]==1)                            //判断是否撞墙以及咬到自己
-            break;
-        else if(bgd[b][a]==3)
-            break;
-        else if(bgd[b][a]==0)                        //蛇的运动
-            {
-                bgd[head->s_y][head->s_x]=0;
-                if(num==0)
-                    {                           
-                        head->s_y=b;
-                        head->s_x=a;
-                    }
-               else                                   //将蛇的末尾部分给头部，达到移动效果
-                    {   
-                        p1=head;
-                        p1->s_y=b;
-                        p1->s_x=a;
-                        head=head->next;
-                        p1->next=NULL;
-                        bgd[p2->s_y][p2->s_x]=3;
-                        p2->next=p1;                       
-                        p2=p1;                                  
-                    }
-                bgd[b][a]=2;
-            }
-        else if(bgd[b][a]==4)                                   //吃到食物，扩展链表
-            {
-                p1=(struct snake*)malloc(SN);
-                p1->s_y=b;
-                p1->s_x=a;
-                p2->next=p1;
-                bgd[p2->s_y][p2->s_x]=3;
-                p2=p1;
-                bgd[b][a]=2;
-                food();
-                num++; 
-            }
+	formal_dir = dir = 's', li = 1;//初始方向向下，生命值为1
+	b = 4, a = 4;
+	num = 0;//num代表得分，也即吃到的食物个数
+	food();
+	while (1)
+	{
+		formal_dir = dir;//这两步用来记住 此时方向，并得到 下一个方向
+		if (_kbhit())
+			dir = _getch();
+		direction();
+		move();
+		change_map2();
+		if (li == 0)
+			break;
+		print1();
+		SetConsoleActiveScreenBuffer(hOutBuf);
+		Sleep(300);
 
-        system("clear");
-                                //打印，0：空位；1：墙；2：蛇头；3：蛇身；4：食物
-        for(y=0;y<20;y++)
-            {                
-                for(x=0;x<20;x++)
-                    {
-                        if(bgd[y][x]==1)
-                            printf("#");
-                        else if(bgd[y][x]==0)
-                            printf(" ");
-                        else if(bgd[y][x]==2)
-                            printf("@");
-                        else if(bgd[y][x]==3)
-                            printf("0");
-                        else if(bgd[y][x]==4)
-                            printf("$");
-                    }                
-                printf("\n");
-            }
+		formal_dir = dir;
+		if (_kbhit())
+			dir = _getch();
+		direction();
+		move();
+		change_map2();
+		if (li == 0)
+			break;
+		print2();
+		SetConsoleActiveScreenBuffer(hOutput);
+		Sleep(300);
 
-            formal_dir=dir;
-            usleep(300000);
-    }
-    printf("GAME OVER\nscore:%d\n",num);
+
+	}
+
+	printf("GAME OVER\nscore:%d\n", num);
+	_getch();
+
+	return 0;
 }
 
+
+
+
 void direction()
-{   if(dir==up&&formal_dir==down)              //防止控制蛇往回走
-        dir=formal_dir;
-    else if(dir==down&&formal_dir==up)
-        dir=formal_dir;
-    else if(dir==left&&formal_dir==right)
-        dir=formal_dir;
-    else if(dir==right&&formal_dir==left)
-        {dir=formal_dir;}
-    switch(dir)                                //蛇四面运动的实现
-    {   case up:b=b-1;break;
-        case down:b=b+1;break;
-        case left:a=a-1;break;
-        case right:a=a+1;break;
-        case 'p':while(1)
-                    {if ('p'==agetch())         //暂停的实现
-                        break;}
-        default:                               //若为其他键输入，则无效化
-        {dir=formal_dir;
-        direction();}
-    }
+{
+	if (dir == up && formal_dir == down)              //防止控制蛇往回走
+		dir = formal_dir;
+	else if (dir == down && formal_dir == up)
+		dir = formal_dir;
+	else if (dir == left && formal_dir == right)
+		dir = formal_dir;
+	else if (dir == right && formal_dir == left)
+		dir = formal_dir;
+	switch (dir)                                //蛇四面运动的实现
+	{
+	case up:b = b - 1; break;
+	case down:b = b + 1; break;
+	case left:a = a - 1; break;
+	case right:a = a + 1; break;
+	case 'p':while (1)
+	{
+		if ('p' == _getch())         //暂停的实现
+			break;
+	}
+	default:                               //若为其他键输入，则无效化
+	{dir = formal_dir;
+	direction(); }
+	}
 }
 
 void food()
 {
-    f1=rand()%18+1;
-    f2=rand()%18+1;
-    if(bgd[f1][f2]==2||bgd[f1][f2]==3||bgd[f1][f2]==1)                           //食物是否和蛇头重合
-        food(); 
-    bgd[f1][f2]=4;
-} 
-
-int agetch()
-{
-     struct termios tm, tm_old;
-     int fd = 0, ch;
-
-     if (tcgetattr(fd, &tm) < 0) {//保存现在的终端设置
-          return -1;
-     }
-
-     tm_old = tm;
-     cfmakeraw(&tm);//更改终端设置为原始模式，该模式下所有的输入数据以字节为单位被处理
-     if (tcsetattr(fd, TCSANOW, &tm) < 0) {//设置上更改之后的设置
-          return -1;
-     }
-
-     ch = getchar();
-     if (tcsetattr(fd, TCSANOW, &tm_old) < 0) {//更改设置为最初的样子
-          return -1;
-     }
-     return ch;
+	if (num != 0) {
+		srand(f1);
+	}
+	f1 = rand() % 18 + 1;
+	if (num != 0) {
+		srand(f2);
+	}
+	f2 = rand() % 18 + 1;
+	if (bgd[f1][f2] == 2 || bgd[f1][f2] == 3 || bgd[f1][f2] == 1)                           //食物是否和蛇头重合
+		food();
+	bgd[f1][f2] = 4;
 }
 
-int kbhit(void)                                
-{  
-      struct termios oldt, newt;  
-      int ch;  
-      int oldf;  
-      tcgetattr(STDIN_FILENO, &oldt);  
-      newt = oldt;  
-      newt.c_lflag &= ~(ICANON | ECHO);  
-      tcsetattr(STDIN_FILENO, TCSANOW, &newt);  
-      oldf = fcntl(STDIN_FILENO, F_GETFL, 0);  
-      fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);  
-      ch = getchar();  
-      tcsetattr(STDIN_FILENO, TCSANOW, &oldt);  
-      fcntl(STDIN_FILENO, F_SETFL, oldf);  
-      if(ch != EOF)  
-      {  
-        ungetc(ch, stdin);  
-        return 1;  
-      }  
-      return 0; 
-} 
+void move() {
+	if (bgd[b][a] == 1)                            //判断是否撞墙以及咬到自己
+		li = 0;
+	else if (bgd[b][a] == 3)
+		li = 0;
+	else if (bgd[b][a] == 0)                        //蛇的运动
+	{
+		bgd[head->s_y][head->s_x] = 0;
+		if (num == 0)
+		{
+			head->s_y = b;
+			head->s_x = a;
+		}
+		else                                   //将蛇的末尾部分给头部，达到移动效果
+		{
+			p1 = head;
+			p1->s_y = b;
+			p1->s_x = a;
+			head = head->next;
+			p1->next = NULL;
+			bgd[p2->s_y][p2->s_x] = 3;
+			p2->next = p1;
+			p2 = p1;
+		}
+		bgd[b][a] = 2;
+	}
+	else if (bgd[b][a] == 4)                                   //吃到食物，扩展链表
+	{
+		p1 = (struct snake*)malloc(SN);
+		p1->s_y = b;
+		p1->s_x = a;
+		p2->next = p1;
+		bgd[p2->s_y][p2->s_x] = 3;
+		p2 = p1;
+		bgd[b][a] = 2;
+		f1 = f1 * dir;
+		f2 = f2 * dir;
+		food();
+		num++;
+	}
+}
+
+void print1() {
+	//打印，0：空位；1：墙；2：蛇头；3：蛇身；4：食物
+	for (y = 0; y < 20; y++){
+		coord.Y = y;
+		WriteConsoleOutputCharacterA(hOutBuf, map2[y], 20, coord, &bytes);
+	}
+}
+void print2() {
+	for (y = 0; y < 20; y++) {
+		coord.Y = y;
+		WriteConsoleOutputCharacterA(hOutput, map2[y], 20, coord, &bytes);
+	}
+}
+
+void change_map2() {
+	for (y = 0; y < 20; y++)
+	{
+		for (x = 0; x < 20; x++)
+		{
+			if (bgd[y][x] == 1)
+				map2[y][x] = '#';
+			else if (bgd[y][x] == 0)
+				map2[y][x] = ' ';
+			else if (bgd[y][x] == 2)
+				map2[y][x] = '@';
+			else if (bgd[y][x] == 3)
+				map2[y][x] = '0';
+			else if (bgd[y][x] == 4)
+				map2[y][x] = '$';
+		}
+	}
+}
